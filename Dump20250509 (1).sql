b94759290bacc7d5e6d8b80c488575e918fc5b7d
Kevin Coll <kevincollmorales@gmail.com>
2025-05-09 19:14:34 +0200

    Base de Datos de SQL

---
+++ Dump20250509 (1).sql
@@ -0,0 +1,104 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: laherrateca
+-- ------------------------------------------------------
+-- Server version	8.4.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contenedor`
+--
+
+DROP TABLE IF EXISTS `contenedor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contenedor` (
+  `codigocontenedor` varchar(20) NOT NULL,
+  `capacidadcontenedor` int NOT NULL,
+  PRIMARY KEY (`codigocontenedor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contenedor`
+--
+
+LOCK TABLES `contenedor` WRITE;
+/*!40000 ALTER TABLE `contenedor` DISABLE KEYS */;
+INSERT INTO `contenedor` VALUES ('AAA-A1-111',10);
+/*!40000 ALTER TABLE `contenedor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `herramienta`
+--
+
+DROP TABLE IF EXISTS `herramienta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `herramienta` (
+  `nombreherramienta` varchar(20) NOT NULL,
+  `codigoherramienta` varchar(20) NOT NULL,
+  `issmall` bit(1) NOT NULL,
+  PRIMARY KEY (`codigoherramienta`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `herramienta`
+--
+
+LOCK TABLES `herramienta` WRITE;
+/*!40000 ALTER TABLE `herramienta` DISABLE KEYS */;
+INSERT INTO `herramienta` VALUES ('Taladro','TAL-BA-001',_binary '');
+/*!40000 ALTER TABLE `herramienta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventario`
+--
+
+DROP TABLE IF EXISTS `inventario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventario` (
+  `codigoherramienta` varchar(20) NOT NULL,
+  `codigocontenedor` varchar(20) NOT NULL,
+  `cantidaddeherramientas` int NOT NULL,
+  PRIMARY KEY (`codigoherramienta`,`codigocontenedor`),
+  KEY `codigoherramienta_idx` (`codigoherramienta`),
+  KEY `codigocontenedor_idx` (`codigocontenedor`),
+  CONSTRAINT `codigocontenedor` FOREIGN KEY (`codigocontenedor`) REFERENCES `contenedor` (`codigocontenedor`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `codigoherramienta` FOREIGN KEY (`codigoherramienta`) REFERENCES `herramienta` (`codigoherramienta`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventario`
+--
+
+LOCK TABLES `inventario` WRITE;
+/*!40000 ALTER TABLE `inventario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-09 19:02:31
